65b1c2086e98f404fb1b6b59dd3a1beb182ad94a
DatGuy <datguysteam@gmail.com>
2025-05-20 00:45:13 +0300

    copy origin header for CORS

--- src/cors.rs
+++ src/cors.rs
@@ -1,6 +1,6 @@
+use rocket::fairing::{Fairing, Info, Kind};
 use rocket::http::Header;
 use rocket::{Request, Response};
-use rocket::fairing::{Fairing, Info, Kind};
 
 pub struct CORS;
 
@@ -9,13 +9,26 @@ impl Fairing for CORS {
     fn info(&self) -> Info {
         Info {
             name: "Add CORS headers to responses",
-            kind: Kind::Response
+            kind: Kind::Response,
         }
     }
 
-    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "GET, POST, OPTIONS"));
-        response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type, Charset, Accept"));
+
+        let origin = request.headers().get_one("Origin");
+        if let Some(origin) = origin {
+            if origin.starts_with("chrome-extension://") {
+                response.set_header(Header::new("Access-Control-Allow-Origin", origin));
+            }
+        }
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "GET, POST, OPTIONS",
+        ));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Headers",
+            "Content-Type, Charset, Accept",
+        ));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,23 @@ extern crate rocket;
 
 use std::collections::HashMap;
 
-use rand::seq::SliceRandom;
+use log::{error, info};
 use rand::SeedableRng;
 
-use aws_config;
-use aws_sdk_polly::{Client, Region};
-use aws_sdk_polly::model::{Engine, LanguageCode, OutputFormat, TextType, VoiceId};
-use rocket::response::status;
-use rocket::response::stream::ReaderStream;
+use aws_config::{self, BehaviorVersion};
+use aws_sdk_polly::types::{Engine, LanguageCode, OutputFormat, TextType, VoiceId};
+use aws_sdk_polly::Client;
+use rand::prelude::IndexedRandom;
+use rocket::response::{status, stream::ReaderStream};
+use rocket::serde::{json::Json, Deserialize};
 use rocket::State;
-use rocket::serde::Deserialize;
-use rocket::serde::json::Json;
 use rocket_governor::{Method, Quota, ReqState, RocketGovernable, RocketGovernor};
-use rocket_validation::{Validate, Validated};
 
 mod cors;
+
+const MAX_IPA_LENGTH: usize = 50;
+const MIN_IPA_LENGTH: usize = 1;
+const RATE_LIMIT_PER_HOUR: u32 = 100;
 
 lazy_static! {
     // Wikipedia IPA language page to AWS LanguageCode
@@ -54,18 +56,20 @@ pub struct RateLimitGuard;
 
 impl<'r> RocketGovernable<'r> for RateLimitGuard {
     fn quota(_method: Method, _route_name: &str) -> Quota {
-        Quota::per_hour(Self::nonzero(100u32))
+        Quota::per_hour(Self::nonzero(RATE_LIMIT_PER_HOUR))
     }
 
-    fn limit_info_allow(_method: Option<Method>, _route_name: Option<&str>, _state: &ReqState) -> bool {
+    fn limit_info_allow(
+        _method: Option<Method>,
+        _route_name: Option<&str>,
+        _state: &ReqState,
+    ) -> bool {
         true
     }
 }
 
-#[derive(Debug, Deserialize, Validate)]
-#[serde(crate = "rocket::serde")]
+#[derive(Debug, Deserialize)]
 pub struct RequestData {
-    #[validate(length(min = 1, max = 50))]
     ipa: String,
     language: String,
 }
@@ -75,35 +79,79 @@ struct Polly {
     speakers: HashMap<String, Vec<VoiceId>>,
 }
 
+impl Polly {
+    async fn synthesize_speech(
+        &self,
+        ipa: &str,
+        voice_id: &VoiceId,
+    ) -> Result<impl rocket::tokio::io::AsyncRead, aws_sdk_polly::Error> {
+        let ssml_text = format!("<phoneme alphabet='ipa' ph='{}'></phoneme>", ipa);
+
+        let resp = self
+            .client
+            .synthesize_speech()
+            .output_format(OutputFormat::OggVorbis)
+            .text(ssml_text)
+            .text_type(TextType::Ssml)
+            .voice_id(voice_id.clone())
+            .send()
+            .await?;
+
+        Ok(resp.audio_stream.into_async_read())
+    }
+}
+
 #[post("/", format = "json", data = "<validated_data>")]
-async fn speak(validated_data: Validated<Json<RequestData>>, polly: &State<Polly>, _limitguard: RocketGovernor<'_, RateLimitGuard>) -> Result<ReaderStream![impl rocket::tokio::io::AsyncRead], status::BadRequest<String>> {
+async fn speak(
+    validated_data: Json<RequestData>,
+    polly: &State<Polly>,
+    _limitguard: RocketGovernor<'_, RateLimitGuard>,
+) -> Result<ReaderStream![impl rocket::tokio::io::AsyncRead], status::BadRequest<String>> {
     let data = validated_data.into_inner();
+
+    // Validate IPA length
+    let ipa_length = data.ipa.len();
+    if ipa_length < MIN_IPA_LENGTH || ipa_length > MAX_IPA_LENGTH {
+        return Err(status::BadRequest(
+            format!("IPA must be between {} and {} characters", MIN_IPA_LENGTH, MAX_IPA_LENGTH),
+        ));
+    }
+
+    // Validate language
     let target_language = &*data.language;
-    if !LANGUAGE_TO_CODE.contains_key(target_language) {
-        return Err(status::BadRequest(Some(format!("Language {target_language} is unsupported"))));
+    let language_code = LANGUAGE_TO_CODE
+        .get(target_language)
+        .ok_or_else(|| status::BadRequest(format!("Language {target_language} is unsupported")))?;
+
+    let generic_language = generic_language_from_code(language_code.clone());
+    let speakers = polly.speakers.get(&generic_language).ok_or_else(|| {
+        status::BadRequest(format!(
+            "No speakers available for language {target_language}"
+        ))
+    })?;
+
+    // Select random speaker
+    let mut rng = rand::rngs::StdRng::from_rng(&mut rand::rng());
+    let random_speaker = speakers
+        .choose(&mut rng)
+        .ok_or_else(|| status::BadRequest("No available speakers".to_string()))?;
+
+    info!(
+        "Synthesizing speech for IPA: {}, language: {}, speaker: {}",
+        data.ipa, target_language, random_speaker
+    );
+
+    // Synthesize speech
+    match polly.synthesize_speech(&data.ipa, random_speaker).await {
+        Ok(audio_stream) => Ok(ReaderStream::one(audio_stream)),
+        Err(e) => {
+            error!("Failed to synthesize speech: {}", e);
+            Err(status::BadRequest(format!(
+                "Failed to synthesize speech: {}",
+                e.to_string()
+            )))
+        }
     }
-
-    let mut rng = rand::rngs::StdRng::from_entropy();
-
-    let generic_language = &*generic_language_from_code(LANGUAGE_TO_CODE.get(target_language).unwrap().clone());
-    if !polly.speakers.contains_key(generic_language) {
-        return Err(status::BadRequest(Some(format!("Language {target_language} is unsupported"))));
-    }
-
-    let random_speaker = polly.speakers.get(generic_language).unwrap().choose(&mut rng).unwrap();
-    let ssml_text = format!("<phoneme alphabet='ipa' ph='{}'></phoneme>", data.ipa);
-
-    let resp = polly.client
-        .synthesize_speech()
-        .output_format(OutputFormat::OggVorbis)
-        .text(ssml_text)
-        .text_type(TextType::Ssml)
-        .voice_id(random_speaker.clone())
-        .send()
-        .await
-        .expect("failed to synthesize speech");
-
-    Ok(ReaderStream::one(resp.audio_stream.into_async_read()))
 }
 
 #[get("/")]
@@ -119,32 +167,52 @@ fn generic_language_from_code(master_code: LanguageCode) -> String {
 }
 
 #[rocket::main]
-async fn main() {
-    let shared_config = aws_config::from_env().region(Region::new("eu-west-2")).load().await;
+async fn main() -> Result<(), rocket::Error> {
+    // Initialize logging
+    env_logger::init();
+    info!("Starting IPA server...");
+
+    let shared_config = aws_config::defaults(BehaviorVersion::latest())
+        .region("eu-west-2")
+        .load()
+        .await;
     let polly_client = Client::new(&shared_config);
 
     let mut all_voices: HashMap<String, Vec<VoiceId>> = HashMap::new();
 
-    let voices_result = polly_client.describe_voices().send().await.expect("Please (re)initialise your AWS credentials. See https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html");
-    for voice in voices_result.voices.unwrap() {
-        if !voice.clone().supported_engines.unwrap().contains(&Engine::Standard) {
+    let voices_result = polly_client
+        .describe_voices()
+        .send()
+        .await
+        .expect("Failed to describe voices - please check AWS credentials");
+
+    for voice in voices_result.voices.unwrap_or_default() {
+        if !voice
+            .clone()
+            .supported_engines
+            .unwrap_or_default()
+            .contains(&Engine::Standard)
+        {
             continue;
         }
 
         let main_language = voice.language_code().unwrap().clone();
-        let mut voice_languages: Vec<LanguageCode> = Vec::from(voice.additional_language_codes().unwrap_or_default());
+        let mut voice_languages: Vec<LanguageCode> = voice.additional_language_codes().to_vec();
         voice_languages.push(main_language);
 
-        // println!("Language for {}: {} ({:#?}). Additional: {:#?}", voice.name().unwrap(), voice.language_name().unwrap(), voice.language_code().unwrap(), voice.additional_language_codes().unwrap_or_default());
-
+        // info!("Language for {}: {} ({:#?}). Additional: {:#?}", voice.name().unwrap(), voice.language_name().unwrap(), voice.language_code().unwrap(), voice.additional_language_codes().unwrap_or_default());
         for voice_language in voice_languages {
-            // Convert to generic language code by taking first two characters.
-            // I hate it but what can you do.
-            let generic_language = generic_language_from_code(voice_language).to_string();
-            // println!("{} speaks {}", voice.name().unwrap(), generic_language);
-            all_voices.entry(generic_language).or_insert(Vec::new()).push(voice.id().unwrap().clone());
+            let generic_language = generic_language_from_code(voice_language);
+            if let Some(voice_id) = voice.id() {
+                all_voices
+                    .entry(generic_language)
+                    .or_insert_with(Vec::new)
+                    .push(voice_id.clone());
+            }
         }
     }
+
+    info!("Loaded {} voice languages", all_voices.len());
 
     let polly = Polly {
         client: polly_client,
@@ -156,7 +224,8 @@ async fn main() {
         .attach(rocket_governor::LimitHeaderGen::default())
         .manage(polly)
         .mount("/", routes![index, speak, all_options])
-        .register("/", catchers![rocket_validation::validation_catcher])
         .launch()
-        .await;
+        .await?;
+
+    Ok(())
 }
